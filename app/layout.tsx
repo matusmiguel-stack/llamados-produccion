82f3aad27df3adc43de898e5fcf5b4972e3e9014
Miguel Matus Soler <miguelmatussoler@MacBook-Air-2.local>
2026-05-25 22:05:55 -0600

    add PWA support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,23 @@
 import "./globals.css"
 
+export const metadata = {
+  title: "Llamados Retro",
+  description: "Sistema de llamados de producción audiovisual",
+  manifest: "/manifest.json",
+  themeColor: "#05070d",
+
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    title: "Llamados Retro",
+  },
+
+  icons: {
+    icon: "/logo-retro.png",
+    apple: "/logo-retro.png",
+  },
+}
+
 export default function RootLayout({
   children,
 }: {
